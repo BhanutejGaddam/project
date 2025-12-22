642de9aa257d7daca86f33d43b76cc72197fbc2a
Gaddam <2460706@cognizant.com>
2025-12-22 12:41:43 +0530

    Home and routes added

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,50 @@
 import { Routes } from '@angular/router';
+import { LoginComponent,canLeaveLoginpage } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DealerComponent } from './dealer/dealer.component';
+import { AdminComponent } from './admin/admin.component';
+import { CustomerComponent } from './customer/customer.component';
+import { customerRoutes } from './customer/customer.routes';
+import { dealerRoutes } from './dealer/dealer.routes';
+import { adminRoutes } from './admin/admin.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:HomeComponent,
+    },
+    {
+        path: 'admin-login',
+        component: LoginComponent,
+        canDeactivate:[canLeaveLoginpage]
+    },
+    {
+        path: 'dealer-login',
+        component: LoginComponent,
+        canDeactivate:[canLeaveLoginpage]
+    },
+    {
+        path: 'customer-login',
+        component: LoginComponent,
+        canDeactivate:[canLeaveLoginpage]
+    },
+    {
+        path:'dealer',
+        component:DealerComponent,
+        children:dealerRoutes
+    },
+    {
+        path:'admin',
+        component:AdminComponent,
+        children:adminRoutes
+    },
+    {
+        path:'customer',
+        component:CustomerComponent,
+        children:customerRoutes
+    },
+    {
+        path:'**',
+        component: HomeComponent
+    },
+];
