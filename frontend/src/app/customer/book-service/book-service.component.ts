0f58b36cb2c4bb6fd61540fb63672b3f9251661e
devansh77718 <Devansh.Kadam@cognizant.com>
2025-12-25 21:35:28 +0530

    Added Service Scheduling Work Flow

--- frontend/src/app/customer/book-service/book-service.component.ts
+++ frontend/src/app/customer/book-service/book-service.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
-
+import { Component, NgModule } from '@angular/core';
+import { bookingDataList, BookingData } from '../../bookingData';
+import { NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-book-service',
-  imports: [],
+  imports: [NgIf, FormsModule],
   templateUrl: './book-service.component.html',
   styleUrl: './book-service.component.css'
 })
-export class BookServiceComponent {
 
+
+export class BookServiceComponent {
+  booking: BookingData = {
+    ownerName: '',
+    phone: '',
+    email: '',
+    address: '',
+    makeModelYear: '',
+    vin: '',
+    registration: '',
+    mileage: 0,
+    fuelType: 'petrol',
+    serviceType: 'routine',
+    issues: '',
+    package: 'basic',
+    history: '',
+    serviceDate: '',
+    pickupDrop: 'none',
+    emergencyContact: '',
+    warranty: 'no',
+    warrantyServices: []
+  };
+
+  message: string = '';
+
+  onSubmit() {
+    // check if serviceDate already exists
+    const exists = bookingDataList.some(
+      b => b.serviceDate === this.booking.serviceDate
+    );
+
+    if (exists) {
+      this.message = 'Choose different time slot';
+    } else {
+      bookingDataList.push({ ...this.booking });
+      this.message = 'Service booked successfully!';
+    }
+  }
 }
